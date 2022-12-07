bc9cca2e4ea9ac5c0d824ec0455ec82b358fc7dc
drewrvaz <drewrvaz@gmail.com>
2022-12-07 14:15:01 -0500

    Began writing base css for the whole app and adding some html to the javascript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/index';
+import Nav from './components/nav/index';
+import About from './components/about/index';
+import Resume from './components/resume/index';
+import Portfolio from './components/portfolio/index';
+import Contact from './components/contact/index';
+import Footer from './components/footer/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Resume />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,17 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <section>
+        <h3>Hello I'm</h3>
+        <h1>Andrew Vazquez</h1>
+        <p>Fullstack Developer</p>
+        <button id='resume'>Download Resume</button>
+        <button id='contact'>Let's Talk</button>
+      </section>
+      <section>
+      </section>
+    </div>
   )
 }
 
