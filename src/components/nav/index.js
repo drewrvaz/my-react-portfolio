4df38dc9ec3988081ce68f86e22806152272df18
drewrvaz <drewrvaz@gmail.com>
2022-12-08 13:59:48 -0500

    Created a floating nav with the help of a youtube video! It's so cool!

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './nav.css'
+import {BiHomeAlt} from 'react-icons/bi'
+import {FiUser} from 'react-icons/fi'
+import {BiBook} from 'react-icons/bi'
+import {MdWorkOutline} from 'react-icons/md'
+import {AiOutlineMessage} from 'react-icons/ai'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><BiHomeAlt/></a>
+      <a href='#about'><FiUser/></a>
+      <a href='#experience'><BiBook/></a>
+      <a href='#portfolio'><MdWorkOutline/></a>
+      <a href='#contact'><AiOutlineMessage/></a>
+    </nav>  
   )
 }
 
