2dac8e2d941139d3a34283ce100caa966fa62cd0
drewrvaz <drewrvaz@gmail.com>
2022-12-08 14:43:25 -0500

    began working on about section, need to push before I log off for a bit

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,7 +3,20 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h3>Get To Know</h3>
+      <h1>Me</h1>
+
+      <div className='containter about-container'>
+        <div className='about-cards'>
+          <article className='about-card'>
+            
+            <h2>Experience</h2>
+            <h5>1 Year Working</h5>
+          </article>
+        </div>
+      </div>
+    </section>
   )
 }
 
