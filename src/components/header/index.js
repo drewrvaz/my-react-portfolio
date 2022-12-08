ff80d54d8e4014fbdcaaf3626c4af789df1a75d2
drewrvaz <drewrvaz@gmail.com>
2022-12-08 11:55:54 -0500

    Finished global css for page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,15 +4,11 @@ import './header.css'
 const Header = () => {
   return (
     <div>
-      <section>
-        <h3>Hello I'm</h3>
+      <h3>Hello I'm</h3>
         <h1>Andrew Vazquez</h1>
         <p>Fullstack Developer</p>
         <button id='resume'>Download Resume</button>
         <button id='contact'>Let's Talk</button>
-      </section>
-      <section>
-      </section>
     </div>
   )
 }
