94fc2e1f47ad8a86e5ccfdd87907bb5776df77c1
drewrvaz <drewrvaz@gmail.com>
2022-12-08 18:32:13 -0500

    Got most of the content and styling for experience done. Working on media queries when I stopped for the night

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -1,9 +1,76 @@
 import React from 'react';
-import './experience.css'
+import './experience.css';
+import {AiOutlineCheckCircle} from 'react-icons/ai';
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Langauges/Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience-container'>
+        <div className='frontend-exp'>
+          <h3>Frontend Development</h3>
+          <div className='experience-content'>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>Bulma</h4>
+              <small className='text-light'>Basic</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>React</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+        <div className='backend-exp'>
+        <h3>Back Development</h3>
+          <div className='experience-content'>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>MongoDB</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>NodeJS</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className='experience-details'>
+              <AiOutlineCheckCircle/>
+              <h4>Express</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
