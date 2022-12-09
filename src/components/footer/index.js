df3c9c8c45f87be321ed55017ba6e47f9cc28c7c
drewrvaz <drewrvaz@gmail.com>
2022-12-09 13:42:30 -0500

    Created and styled the footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
+import {SiLinkedin} from 'react-icons/si';
+import {FiGithub} from 'react-icons/fi';
+import {BsStackOverflow} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='footer-socials'>
+        <a href='https://www.linkedin.com/in/andrew-vazquez-4079bb63/' target="_blank"><SiLinkedin/></a>
+        <a href='https://github.com/drewrvaz' target="_blank"><FiGithub/></a>
+        <a href='https://stackoverflow.com/?newreg=c1e033ee5eb34825867ac18a38fd409b' target="_blank"><BsStackOverflow/></a>
+      </div>
+    </footer>
   )
 }
 
